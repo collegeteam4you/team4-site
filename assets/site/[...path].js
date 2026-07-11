03858e59243491ddb5a7809e7a01c77b32330dee
collegeteam4you <collegeteam4you@gmail.com>
2026-07-11 11:13:44 +0400

    Update [...path].js

--- assets/site/[...path].js
+++ assets/site/[...path].js
@@ -355,13 +355,14 @@ const handleManualPaymentApi = async (req, res, pathname) => {
     sendJson(res, 200, {
       ok: true,
       order: rowToOrder(result.rows[0]),
-      bankDetails: {
-        receiver: manualPaymentReceiver,
-        account: manualPaymentAccount,
-        amount: bookPriceGel,
-        currency: 'GEL',
-        purpose: paymentCode,
-      },
+bankDetails: {
+  receiver: manualPaymentReceiver,
+  bogAccount: manualPaymentAccount,
+  tbcAccount: manualPaymentTbcAccount,
+  amount: bookPriceGel,
+  currency: 'GEL',
+  purpose: paymentCode,
+},
     });
     return;
   }
