141e68b2622a98ff825a5dbc9b2f88bf780e9992
collegeteam4you <collegeteam4you@gmail.com>
2026-07-12 19:01:30 +0400

    Update team4-app.js

--- assets/team4-app.js
+++ assets/team4-app.js
@@ -2070,7 +2070,6 @@ function App() {
     return h(LibraryPage, { lang, setLang });
   }
 
-  return h(React.Fragment, null, h('div', { className: 'luxury-light-field', 'aria-hidden': 'true' }), h(Header, { lang, setLang }), h('main', null, h(Hero, { lang }), h(About, { lang }), h(Courses, { lang }), h(BookGallery, { lang }), h(Programs, { lang }), h(WinSpaceSection, { lang }), h(Testimonials, { lang }), h(Contact, { lang })), h(Footer, { lang }));
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(h(App));
