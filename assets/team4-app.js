ce24516240c3bd49be8a8523a2b828e4d5a5f124
collegeteam4you <collegeteam4you@gmail.com>
2026-08-11 15:54:19 +0400

    Update team4-app.js

--- assets/team4-app.js
+++ assets/team4-app.js
@@ -3556,11 +3556,23 @@ hairFemale: [
                 },
               },
 
-              avatarLayer(
+            avatarLayer(
   '/assets/avatar-v2/base/' +
     gender +
     '-base.png',
-  1
+  1,
+  {
+    width: '100%',
+    height: '100%',
+
+    left: gender === 'male' ? '-12%' : '0%',
+    top: '0',
+
+    right: 'auto',
+    bottom: 'auto',
+
+    objectFit: 'contain',
+  }
 ),
 
               avatarLayer(
