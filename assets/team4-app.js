27c5b72645d317ebab5884d16df4afea2cd476df
collegeteam4you <collegeteam4you@gmail.com>
2026-08-12 15:46:55 +0400

    Update team4-app.js

--- assets/team4-app.js
+++ assets/team4-app.js
@@ -2639,7 +2639,7 @@ function Team4AvatarPage({ lang, setLang }) {
 const hairAdjustments = {
   male: {
     'hair-m-01': { width: '29%', top: '0.8%', x: '0%' },
-    'hair-m-02': { width: '31%', top: '0.1%', x: '0%' },
+    'hair-m-02': { width: '28%', top: '0.1%', x: '0%' },
     'hair-m-03': { width: '29%', top: '0.1%', x: '0%' },
     'hair-m-04': { width: '29%', top: '0.1%', x: '0%' },
     'hair-m-05': { width: '29%', top: '0.1%', x: '0%' },
