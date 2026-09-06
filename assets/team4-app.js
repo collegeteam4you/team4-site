644db043d16207103ea86e631af40b2c6ac010ec
collegeteam4you <collegeteam4you@gmail.com>
2026-09-06 17:38:55 +0400

    Update team4-app.js

--- assets/team4-app.js
+++ assets/team4-app.js
@@ -2338,10 +2338,8 @@ function Team4LabPage({ lang, setLang }) {
       {
         style: {
           minHeight: '100vh',
-
-          padding:
-            '150px 24px 80px',
-
+padding:
+  '105px 24px 24px',
           background:
             'radial-gradient(circle at 20% 20%, rgba(239,27,19,0.18), transparent 35%), radial-gradient(circle at 80% 30%, rgba(29,92,255,0.18), transparent 35%), #030305',
 
