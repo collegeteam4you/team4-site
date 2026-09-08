73120142a3f64b57174d2886235667cdcf6042d1
XURCO <collegeteam4you@gmail.com>
2026-09-08 13:22:08 +0400

    Update team4-app.js

--- assets/team4-app.js
+++ assets/team4-app.js
@@ -5413,7 +5413,7 @@ function renderJobAvatar() {
     h('model-viewer', {
       src:
         avatar.gender === 'female'
-          ? '/assets/team4-lab/avatar-3d/TEAM4_FEMALE_MASTER.glb'
+         '/assets/team4-lab/avatar-3d/TEAM4_FEMALE_WALK.glb'
           : '/assets/team4-lab/avatar-3d/TEAM4_MALE_WALK.glb',
 
       alt: 'Team4 Avatar',
